bbf0546d698608107a0fbf1ef231c48aacd64329
VincentWardhana <vwcoolz@yahoo.com>
2017-12-07 15:54:04 +0700

    a

--- application/config/database.php
+++ application/config/database.php
@@ -73,8 +73,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'rs7sqkrnlz.database.windows.net';
-$db['default']['username'] = 'purwa@rs7sqkrnlz';
+$db['default']['hostname'] = 'neuromotor.database.windows.net';
+$db['default']['username'] = 'purwa';
 $db['default']['password'] = 'db@neurosoft2014';
 $db['default']['database'] = 'neuromotor2';
 $db['default']['port'] = 1433;
