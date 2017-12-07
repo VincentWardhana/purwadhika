cc66970f8328e7cf030b690bb2e454065dcf0064
VincentWardhana <vwcoolz@yahoo.com>
2017-12-07 14:04:03 +0700

    a

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'tcp:neuromotor.database.windows.net,1433',
+	'username' => 'purwa',
+	'password' => 'db@neurosoft2014',
+	'database' => 'Neuromotor',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
